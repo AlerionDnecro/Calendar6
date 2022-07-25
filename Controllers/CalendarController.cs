82dc254ab3effd96f872b19a62d9b07403e07419
AlerionDnecro <74513232+AlerionDnecro@users.noreply.github.com>
2022-07-25 12:30:11 -0500

    Initial commit

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,75 @@
+﻿using MyCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MyCalendar.Controllers
+{
+    public class CalendarActionResponseModel
+    {
+        public String Status;
+        public Int64 Source_id;
+        public Int64 Target_id;
+
+        public CalendarActionResponseModel(String status, Int64 source_id, Int64 target_id)
+        {
+            Status = status;
+            Source_id = source_id;
+            Target_id = target_id;
+        }
+    }
+
+    public class CalendarController : Controller
+    {
+        //
+        // GET: /Calendar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            MyEventsDataContext data = new MyEventsDataContext();
+            return View(data.Events);
+        }
+
+		public ActionResult Save(Event changedEvent, FormCollection actionValues)
+		{
+			String action_type = actionValues["!nativeeditor_status"];
+			Int64 source_id = Int64.Parse(actionValues["id"]);
+			Int64 target_id = source_id;
+				
+	
+			MyEventsDataContext data = new MyEventsDataContext();
+			try{
+				switch (action_type)
+				{
+					case "inserted":
+                        data.Events.InsertOnSubmit(changedEvent);
+						break;
+					case "deleted":
+                        changedEvent = data.Events.SingleOrDefault(ev => ev.id == source_id);
+                        data.Events.DeleteOnSubmit(changedEvent);
+						break;
+					default: // "updated"
+                        changedEvent = data.Events.SingleOrDefault(ev => ev.id == source_id);
+                        UpdateModel(changedEvent);
+						break;
+				}
+				data.SubmitChanges();
+                target_id = changedEvent.id;
+			}
+			catch
+			{
+				action_type = "error";
+			}
+	
+			return View(new CalendarActionResponseModel(action_type, source_id, target_id));
+		}
+	}
+}
---
+++ Views/Calendar/Index.aspx
@@ -0,0 +1,45 @@
+﻿	<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" >
+	<head id="Head1" runat="server">
+	    <title>Index</title>
+	    <script src="/Scripts/dhtmlxscheduler.js" type="text/javascript"></script>
+	    <link href="/Scripts/dhtmlxscheduler.css" rel="stylesheet" type="text/css" />
+	    <style type="text/css">
+	        html, body
+	        {
+	            height:100%;
+	            padding:0px;
+	            margin:0px;
+	        }
+	    </style>
+	    <script type="text/javascript">
+	        function init() {
+	            scheduler.config.xml_date = "%m/%d/%Y %H:%i";
+	            scheduler.init("scheduler_here", new Date(2010, 6, 1), "month");
+	            scheduler.load("/Calendar/Data");
+
+	            var dp = new dataProcessor("/Calendar/Save");
+	            dp.init(scheduler);
+	            dp.setTransactionMode("POST", false);
+	        }
+	    </script>
+	</head>
+	<body onload="init()">
+		<div id="scheduler_here" class="dhx_cal_container" style='width:100%; height:100%;'>
+			<div class="dhx_cal_navline">
+				<div class="dhx_cal_prev_button">&nbsp;</div>
+				<div class="dhx_cal_next_button">&nbsp;</div>
+				<div class="dhx_cal_today_button"></div>
+				<div class="dhx_cal_date"></div>
+				<div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
+				<div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
+				<div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
+			</div>
+			<div class="dhx_cal_header">
+			</div>
+			<div class="dhx_cal_data">
+			</div>		
+		</div>
+	</body>
+	</html>
